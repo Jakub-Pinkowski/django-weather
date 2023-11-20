6b0710b944214a847a235e4b5b01241d6c741240
Jakub_Pinkowski <pinkowski.jakub@gmail.com>
2023-11-20 14:40:16 +0100

    Refactor weather_app/views.py

--- weather_app/views.py
+++ weather_app/views.py
@@ -7,34 +7,62 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
     API_KEY = open("AIP_KEY", "r").read()
-    current_weather_url = "https://api.openweathermap.org/data/2.5/weather?q={}&appid={}"
-    forecast_url = "https://api.openweathermap.org/data/2.5/forecast?lat={}&lon={}&appid={}"
+    current_weather_url = (
+        "https://api.openweathermap.org/data/2.5/weather?q={}&appid={}"
+    )
+    forecast_url = (
+        "https://api.openweathermap.org/data/2.5/forecast?lat={}&lon={}&appid={}"
+    )
 
     if request.method == "POST":
-        city1 = request.POST['city1']
-        city2 = request.get('city2', None)
+        city1 = request.POST["city1"]
+        city2 = request.get("city2", None)
+
+        weather_data1, daily_forecast1 = fetch_weather_and_forecast(
+            city1, API_KEY, current_weather_url, forecast_url
+        )
+
+        if city2:
+            weather_data2, daily_forecast2 = fetch_weather_and_forecast(
+                city2, API_KEY, current_weather_url, forecast_url
+            )
+        else:
+            weather_data2, daily_forecast2 = None, None
+
+        return render(
+            request,
+            "weather_app/index.html",
+            {
+                "weather_data1": weather_data1,
+                "daily_forecast1": daily_forecast1,
+                "weather_data2": weather_data2,
+                "daily_forecast2": daily_forecast2,
+            },
+        )
     else:
         return render(request, "weather_app/index.html")
-    
+
 
 def fetch_weather_and_forecast(city, api_key, current_weather_url, forecast_url):
     response = requests.get(current_weather_url.format(city, api_key)).json()
-    lat, lon = response['coord']['lat'], response['coord']['lon']
+    lat, lon = response["coord"]["lat"], response["coord"]["lon"]
     forecast_response = requests.get(forecast_url.format(lat, lon, api_key)).json()
 
     weather_data = {
         "city": city,
-        "tempature": round(response['main']['temp'] - 273.15, 2),
-        "description": response['weather'][0]['description'],
-        "icon": response['weather'][0]['icon'],
+        "tempature": round(response["main"]["temp"] - 273.15, 2),
+        "description": response["weather"][0]["description"],
+        "icon": response["weather"][0]["icon"],
     }
 
     daily_forecast = []
-    for day in forecast_response['list']:
-        if day['dt_txt'].split()[1] == "12:00:00":
-            daily_forecast.append({
-                "date": day['dt_txt'].split()[0],
-                "tempature": round(day['main']['temp'] - 273.15, 2),
-                "description": day['weather'][0]['description'],
-                "icon": day['weather'][0]['icon'],
-            })
+    for day in forecast_response["list"]:
+        if day["dt_txt"].split()[1] == "12:00:00":
+            daily_forecast.append(
+                {
+                    "date": day["dt_txt"].split()[0],
+                    "tempature": round(day["main"]["temp"] - 273.15, 2),
+                    "description": day["weather"][0]["description"],
+                    "icon": day["weather"][0]["icon"],
+                }
+            )
