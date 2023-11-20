2c712afdc3fb2a8aea5df382cb4d85c7feedb1a1
Jakub_Pinkowski <pinkowski.jakub@gmail.com>
2023-11-20 14:58:57 +0100

    Fix typo in API key file name

--- weather_app/views.py
+++ weather_app/views.py
@@ -6,7 +6,7 @@ from django.shortcuts import render
 
 # Create your views here.
 def index(request):
-    API_KEY = open("AIP_KEY", "r").read()
+    API_KEY = open("API_KEY", "r").read()
     current_weather_url = (
         "https://api.openweathermap.org/data/2.5/weather?q={}&appid={}"
     )
