1ec06d9d9f392d311a61584d53fe5fcd46e60a8e
Jakub_Pinkowski <pinkowski.jakub@gmail.com>
2023-11-20 13:56:56 +0100

    Add API integration for current weather and forecast

--- weather_app/views.py
+++ weather_app/views.py
@@ -1,3 +1,16 @@
+import datetime
+import requests
+
 from django.shortcuts import render
 
+
 # Create your views here.
+def index(request):
+    API_KEY = open("AIP_KEY", "r").read()
+    current_weather_url = "https://api.openweathermap.org/data/2.5/weather?q={}&appid={}"
+    forecast_url = "https://api.openweathermap.org/data/2.5/forecast?lat={}&lon={}&appid={}"
+
+    if request.method == "POST":
+        pass
+    else:
+        return render()
