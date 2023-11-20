a99f7d8caa62965b6b0763608edce0e0288cdcd7
Jakub_Pinkowski <pinkowski.jakub@gmail.com>
2023-11-20 14:28:17 +0100

    Add weather data fetching functionality

--- weather_app/views.py
+++ weather_app/views.py
@@ -11,6 +11,19 @@ def index(request):
     forecast_url = "https://api.openweathermap.org/data/2.5/forecast?lat={}&lon={}&appid={}"
 
     if request.method == "POST":
-        pass
+        city1 = request.POST['city1']
+        city2 = request.get('city2', None)
     else:
-        return render()
+        return render(request, "weather_app/index.html")
+    
+
+def fetch_weather_and_forecast(city, api_key, current_weather_url, forecast_url):
+    response = requests.get(current_weather_url.format(city, api_key)).json()
+    lat, lon = response['coord']['lat'], response['coord']['lon']
+    forecast_response = requests.get(forecast_url.format(lat, lon, api_key)).json()
+
+    weather_data = {
+        "city": city,
+        "tempature": round(response['main']['temp'] - 273.15, 2),
+        "description": response['weather'][0]['description'],
+    }
