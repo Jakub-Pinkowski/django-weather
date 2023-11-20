456c5787b88f5e5e9ae6d942752f8fbd81c98955
Jakub_Pinkowski <pinkowski.jakub@gmail.com>
2023-11-20 15:05:56 +0100

    Fix variable name and add print statement

--- weather_app/views.py
+++ weather_app/views.py
@@ -16,24 +16,25 @@ def index(request):
 
     if request.method == "POST":
         city1 = request.POST["city1"]
-        city2 = request.get("city2", None)
+        city2 = request.POST.get("city2", None)
 
-        weather_data1, daily_forecast1 = fetch_weather_and_forecast(
+        weather_data1, daily_forecasts1 = fetch_weather_and_forecast(
             city1, API_KEY, current_weather_url, forecast_url
         )
+        print(weather_data1)
 
         if city2:
-            weather_data2, daily_forecast2 = fetch_weather_and_forecast(
+            weather_data2, daily_forecasts2 = fetch_weather_and_forecast(
                 city2, API_KEY, current_weather_url, forecast_url
             )
         else:
-            weather_data2, daily_forecast2 = None, None
+            weather_data2, daily_forecasts2 = None, None
 
-        context: {
+        context = {
             "weather_data1": weather_data1,
-            "daily_forecast1": daily_forecast1,
+            "daily_forecasts1": daily_forecasts1,
             "weather_data2": weather_data2,
-            "daily_forecast2": daily_forecast2,
+            "daily_forecasts2": daily_forecasts2,
         }
 
         return render(request, "weather_app/index.html", context)
